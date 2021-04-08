71aa377ab890623d3a6f267eeb7dcdbee5b55535
Sandeepan-Das <sandeepandas2000@gmail.com>
2021-04-08 19:14:44 +0530

    checkBox added

--- frontEnd2/projects/mockTest/src/app/shared-questionbank/shared-questionbank.component.ts
+++ frontEnd2/projects/mockTest/src/app/shared-questionbank/shared-questionbank.component.ts
@@ -1,30 +1,50 @@
-
 import { Component, OnInit } from '@angular/core';
 import { OnlineTestService } from '../online-test.service';
 
 @Component({
   selector: 'app-shared-questionbank',
   templateUrl: './shared-questionbank.component.html',
-  styleUrls: ['./shared-questionbank.component.css']
+  styleUrls: ['./shared-questionbank.component.css'],
 })
 export class SharedQuestionbankComponent implements OnInit {
-
   public display_data = [];
-  constructor(private service:OnlineTestService) { }
+  constructor(private service: OnlineTestService) {}
+  public questinArray = [];
+  public Level: String;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  difficultyLevel(event) {
+    this.display_data = [];
+    this.Level = event.target.value;
+    console.log(this.Level);
+    this.service.Fetchallquestion(event.target.value).subscribe((data) => {
+      data.forEach((element) => {
+        // console.log(element)
+        this.display_data.push(element);
+      });
+    });
   }
-  difficultyLevel(event){
-    this.display_data=[];
-    
-    this.service.Fetchallquestion(event.target.value).subscribe((data)=>{
-        data.forEach(element => {
-          console.log(element)
-          this.display_data.push(element);
-        });
-    })
-    
+ 
 
+  checkbox() {
+    this.questinArray = this.getTheid();
+    if (this.Level == 'Easy') {
+      
+    } else if (this.Level == 'Medium') {
+    } else if (this.Level == 'Difficult') {
+    }
+   console.log(this.questinArray);
+   
+    
   }
-
+  public getTheid(){
+   var Arr = [];
+    for (let index = 0; index < this.display_data.length; index++) {
+      var htmlElem = <HTMLInputElement>(
+        document.getElementById(index.toString())
+      );
+      if (htmlElem.checked) Arr.push(htmlElem.value);
+    }
+    return Arr;
+  }
 }
--- frontEnd2/projects/mockTest/src/app/view-questions/view-questions.component.ts
+++ frontEnd2/projects/mockTest/src/app/view-questions/view-questions.component.ts
@@ -9,13 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class ViewQuestionsComponent implements OnInit {
 
   public display_data = [];
+  public questinArray = [];
+  public Level;
   constructor(private service:OnlineTestService) { }
 
   ngOnInit(): void {
   }
   difficultyLevel(event){
     this.display_data=[];
-    
+    this.Level = event.target.value;
     this.service.Fetchquestion(event.target.value).subscribe((data)=>{
         data.forEach(element => {
           console.log(element)
@@ -24,5 +26,26 @@ export class ViewQuestionsComponent implements OnInit {
     })
 
   }
-
+  checkbox() {
+    this.questinArray = this.getTheid();
+    if (this.Level == 'Easy') {
+      
+    } else if (this.Level == 'Medium') {
+    } else if (this.Level == 'Difficult') {
+    }
+   console.log(this.questinArray);
+   
+    
+  }
+  public getTheid(){
+   var Arr = [];
+    for (let index = 0; index < this.display_data.length; index++) {
+      var htmlElem = <HTMLInputElement>(
+        document.getElementById(index.toString())
+      );
+      if (htmlElem.checked) Arr.push(htmlElem.value);
+    }
+    return Arr;
+  }
+ 
 }
